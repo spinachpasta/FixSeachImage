f946e1f5012dcd4a278db5239b073a0cddc332c5
spinachpasta <49642352+spinachpasta@users.noreply.github.com>
2021-12-27 22:36:18 +0900

    query test code

--- override.js
+++ override.js
@@ -1,7 +1,27 @@
-var atags = document.getElementsByTagName('a');
+const urlParams = new URLSearchParams(window.location.search);
+// console.log(window.location.origin);
+const q = urlParams.get('q');
+const origin = window.location.origin;
+const atags = document.getElementsByTagName('a');
+
+
 for (var i = 0; i < atags.length; i++) {
 	var a = atags[i];
-	if(a.innerText=="All"){
-		a.innerText="All2";
+	const params = new URLSearchParams(a.href);
+	if (params.has("tbm")) {
+		console.log(params.get("tbm"));
 	}
 }
+
+//all books videos news images maps shopping flights finance
+
+/*
+bks
+nws
+vid
+lcl
+nws
+isch
+flm
+shop
+*/
